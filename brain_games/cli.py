5a67474664d2e66de0aa2f8d92530c561a15c644
Heavybrain <mrsaanisimov@mail.ru>
2024-08-18 20:30:58 +0300

    Add flake8 and remove mistakes

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,6 @@
-import prompt
-
 
 def welcome_user():
-    print ("Welcom to the Brain Games!")
+    print("Welcom to the Brain Games!")
     name = input('May I have Your name? ')
-    print (f"Hello, {name}!")
+    print(f"Hello, {name}!")
     return name
-
-
