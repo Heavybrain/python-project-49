7395e68a719851aa14bc4130811acfb3a4dfbf64
Heavybrain <mrsaanisimov@mail.ru>
2024-08-22 21:29:44 +0300

    Stage 5 comleted

---
+++ brain_games/games/even_game.py
@@ -0,0 +1,40 @@
+# Игра: "проверка на четность"
+#  1. Вывод случайного целого числа на экран
+#  2. Пользователь отвечает yes/no
+#  3. Если неверный ответ - игра завершается и выводится сообщение о проигрышше
+#  4. Если ответ верный - следующее число
+#  5. Игра до 3 правильных ответов подряд
+#  6. Вывод поздравления
+
+#!/usr/bin/env python3
+
+import random
+import sys
+sys.path.append('/home/heavybrain/projects/python-project-49/brain_games')
+from ..cli import welcome_user
+
+name = welcome_user()
+print('Answer "yes" if the number is even, otherwise answer "no"')
+
+
+def game():
+    counter = 0
+    while counter < 3:
+        random_number = random.randint(1,100)
+        print(f"Question: {random_number}")
+        user_input = input("Your answer: ").lower()
+        if  random_number % 2 == 0 and user_input =='yes':
+            counter += 1
+            print('Correct!')
+        elif random_number % 2 != 0 and user_input == 'no':
+            counter += 1
+            print('Correct!')
+        elif random_number % 2 == 0 and user_input == "no":
+            print("'no' is wrong answer;(. Correct answer was 'yes'")
+            print(f"Let's try again,{name}!")
+            return
+        elif random_number % 2 != 0 and user_input == "yes":
+            print("'yes' is wrong answer;(. Correct answer was 'no'")
+            print(f"Let's try again,{name}!")
+            return
+    print(f'Congratulations,{name}!')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,39 +1,10 @@
-# Игра: "проверка на четность"
-#  1. Вывод случайного целого числа на экран
-#  2. Пользователь отвечает yes/no
-#  3. Если неверный ответ - игра завершается и выводится сообщение о проигрышше
-#  4. Если ответ верный - следующее число
-#  5. Игра до 3 правильных ответов подряд
-#  6. Вывод поздравления
-
 #!/usr/bin/env python3
-
-import random
 import sys
-sys.path.append('brain_games')
-from cli.py import welcome_user
-# from .brain_games.cli import welcome_user
+sys.path.append('/home/heavybrain/projects/python-project-49/brain_games/')
+from brain_games.games.even_game import game
 
-print('Answer "yes" if the number is even, otherwise answer "no"')
-def game():
-    counter = 0
-    while counter < 3:
-        random_number = random.randint(1,)
-        print(f"Question: {random_number}")
-        user_input = input("Your answer: ").lower()
-        if  random_number % 2 == 0 and user_input == 'yes':
-            counter += 1
-            print('Correct!')
-        elif random_number % 2 != 0 and user_input == 'no':
-            counter += 1
-            print('Correct!')
-        elif random_number % 2 == 0 and user_input == "no":
-            print("'no' is wrong answer;(. Correct answer was 'yes'")
-            print(f"Let's try again,{name}!")
-            return
-        elif random_number % 2 != 0 and user_input == "yes":
-            print("'yes' is wrong answer;(. Correct answer was 'no'")
-            print(f"Let's try again,{name}!")
-            return
-    print(f'Congratulations,{name}!')
+def main():
+    game()
 
+if __name__ == '__main__':
+    main()
