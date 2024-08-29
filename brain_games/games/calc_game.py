47539340a7833f5184f5a03d9d06b0a9b3a5c6ae
Heavybrain <mrsaanisimov@mail.ru>
2024-08-29 18:35:01 +0300

    add 6 stage of project, add brain-calc game

---
+++ brain_games/games/calc_game.py
@@ -0,0 +1,47 @@
+# Игра: "калькулятор"
+#  1. Вывод случайного математического выражения на экран
+#  2. Пользователь делает вычисления и вводит ответ
+#  3. Если неверный ответ - игра завершается и выводится сообщение о проигрышше
+#  4. Если ответ верный - следующее число
+#  5. Игра до 3 правильных ответов подряд
+#  6. Вывод поздравления
+
+#!/usr/bin/env python3
+
+import random
+import sys
+sys.path.append('/home/heavybrain/projects/python-project-49/brain_games')
+from ..cli import welcome_user
+
+name = welcome_user()
+print('What is the result of the expression?')
+
+def generate_expression():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    operator = random.choice(['+', '-', '*'])
+    
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    else:
+        result = num1 * num2
+    return f"{num1} {operator} {num2}", result
+
+
+
+def calculator():
+    counter = 0
+    while counter < 3:
+        expression, correct_answer = generate_expression()
+        print(f"Question: {expression}")
+        user_input = input("Your answer: ")
+        if  int(user_input) == int(correct_answer):
+            counter += 1
+            print('Correct!')
+        else :
+            print(f"{user_input} is wrong answer;(. Correct answer was {correct_answer}")
+            print(f"Let's try again, {name}!")
+            return
+    print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('/home/heavybrain/projects/python-project-49/brain_games/')
+from brain_games.games.calc_game import calculator
+
+def main():
+    calculator()
+
+if __name__ == '__main__':
+    main()
