2877afbe7a5043a70b0842c1a855b8793c596d89
Heavybrain <mrsaanisimov@mail.ru>
2024-09-05 14:06:33 +0300

    add e in welcome for greeting

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,6 @@
 
 def welcome_user():
-    print("Welcom to the Brain Games!")
+    print("Welcome to the Brain Games!")
     name = input('May I have Your name? ')
     print(f"Hello, {name}")
     return name
