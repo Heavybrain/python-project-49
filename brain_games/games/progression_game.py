af7bbd4fd2003b17a97f3ca8d629e16f7613edb8
Heavybrain <mrsaanisimov@mail.ru>
2024-09-09 17:09:20 +0300

    fix progression_game.py and brain_progression.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -6,9 +6,9 @@ QUESTION = 'What number is missing in the progression?'
 
 def get_question_and_answer():
     start_num = random.randint(1, 100)
-    step = random.randint(1, 100)
+    step = random.randint(1, 20)
     miss_num = random.randint(0, 9)
-    progression = list(range(start_num, 150, step))[:10]
+    progression = list(range(start_num, 1000, step))[:10]
     hidden_number = progression[miss_num]
     progression[miss_num] = '..'
     new_task = ' '.join(map(str, progression))
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.games.logic import play_game
-from brain_games.games.progression_game.py import get_question_and_answer
-from brain_games.games.progression_game.py import QUESTION
+from brain_games.games.progression_game import get_question_and_answer
+from brain_games.games.progression_game import QUESTION
 
 
 def main():
